d28e075ae8f7519e9607f964ff1f789cc94d43e1
Youcef Sabri <rsg951@gmail.com>
2024-01-21 03:32:55 +0100

    Added game logic to the game

--- index.js
+++ index.js
@@ -15,34 +15,64 @@ function getComputerChoice() {
 
 // playGames takes 2 args, playerChoice and cpuChoice
 // args must be case-insensitive and
-function playOneGame(player, computerChoice) {
-	// set playerChoice to lowerCase
-	let playerChoice = player.toLowerCase();
+function playOneGame(playerChoice, computerChoice) {
 	// while loop until a winner is decided
 	// compare playerChoice to computerChoice.
 	if (playerChoice === computerChoice) {
+		console.log("Draw.");
 		return "draw";
 	} else if (playerChoice === "rock") {
-		if (computerChoice === "scissors") return "win";
-		if (computerChoice === "paper") return "lost";
+		if (computerChoice === "scissors") {
+			console.log("You Win! Rock beats Scissors");
+			return "win";
+		}
+		if (computerChoice === "paper") {
+			console.log("You Lose! Paper beats Rock");
+			return "lost";
+		}
 	} else if (playerChoice === "paper") {
-		if (computerChoice === "rock") return "win";
-		if (computerChoice === "scissors") return "lost";
+		if (computerChoice === "rock") {
+			console.log("You Win! Paper beats Rock");
+			return "win";
+		}
+		if (computerChoice === "scissors") {
+			console.log("You Lose! Scissors beats Paper");
+			return "lost";
+		}
 	} else {
-		if (computerChoice === "paper") return "win";
-		if (computerChoice === "rock") return "lost";
+		if (computerChoice === "paper") {
+			console.log("You Win! Scissors beats Paper");
+			return "win";
+		}
+		if (computerChoice === "rock") {
+			console.log("You Lose! Rock beats Scissors");
+			return "lost";
+		}
 	}
 }
 
 // fnc that plays a bo5
 function game() {
 	// set a var to score for human and cpu
+	let cpuScore = 0;
+	let userScore = 0;
 	// for loop until 5
-	// if/else call previous fnc to get a winner
-	// if true, increase player score
-	// .log winner of round
-	// else increase cpu score
-	// .log winner
+	while (true) {
+		// ask user for input
+		let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+		let cpuChoice = getComputerChoice();
+		// check who won the game
+		let checkWinner = playOneGame(playerChoice, cpuChoice);
+		// if true, increase player score
+		if (checkWinner === "win") userScore++;
+		// else increase cpu score
+		else if (checkWinner === "lost") cpuScore++;
+		if (userScore === 5 || cpuScore === 5) break;
+	}
 	// check winner with comparaison of var score
-	// return str with winner of bo5
+	if (userScore > cpuScore) {
+		return `You won the game! ${userScore} - ${cpuScore}`;
+	} else {
+		return `You lost the game! ${userScore} - ${cpuScore}`;
+	}
 }
